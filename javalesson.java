5e9d244d0f98d2b7b1bfeebfecc9594bc66d98ba
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-23 01:06:31 +0900

    java初級編_1

---
+++ javalesson.java
@@ -0,0 +1,9 @@
+class javalesson{
+  public static void main(String[]ages){
+    int num1 = 32;
+    int num2 = 5;
+    int num3 = num1/num2;
+    System.out.println(num3);
+    System.out.println(num1*num3);
+  }
+}

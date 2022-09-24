ed808758e7b53beae07db442a9c03dcba4282f65
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-25 00:17:49 +0900

    java初級編_11

---
+++ javalesson12.java
@@ -0,0 +1,11 @@
+public class javalesson12 {
+  public static void main(String[] ages) {
+    for (int i = 0; i <= 20; i++) {
+      if (i % 5 == 0) {
+        System.out.println("bar");
+      } else {
+        System.out.println(i);
+      }
+    }
+  }
+}

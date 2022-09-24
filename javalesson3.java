ddead729c496a8831eada4d52265ba93e59ceeac
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-24 21:35:55 +0900

    java初級編_3

---
+++ javalesson3.java
@@ -0,0 +1,9 @@
+public class javalesson3 {
+  public static void main(String[]ages){
+    System.out.println("練習２問目");
+    int num = 10;
+    if(num <= -10 || num >= 10){
+      System.out.println("おｋ");
+    }
+  }
+}

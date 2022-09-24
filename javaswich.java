00d79f3d39ca28430a6fb9d44fedcbace4936f3e
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-24 23:34:27 +0900

    switch_lesson

---
+++ javaswich.java
@@ -0,0 +1,18 @@
+public class javaswich {
+  public static void main(String[]ages){
+    int num1=3;
+    switch(num1){
+      case 1:
+        System.out.println("one");
+         break;
+      case 2:
+        System.out.println("two");
+         break;
+      case 3:
+        System.out.println("three");
+         break;
+      default:
+      System.out.println("マッチしない");
+    }
+  }
+}

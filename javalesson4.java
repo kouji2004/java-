409355ebaf5e10e2f3cac1a3f86bd8a43eccff06
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-24 23:03:02 +0900

    java初級編_3

---
+++ javalesson4.java
@@ -0,0 +1,9 @@
+public class javalesson4 {
+  public static void main(String[]ages){
+    int num = 10;
+    if((num>-10 && num<0) ||(num>=10)){
+      System.out.println("ok");
+    }
+    System.out.println("ng");
+  }
+}

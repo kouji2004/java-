eb21fdaed49c7596f7303fdf44b1340fdba75037
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-25 01:13:17 +0900

    java初級編_15

---
+++ javalesson17.java
@@ -0,0 +1,8 @@
+public class javalesson17 {
+  public static void main(String[]ages){
+    int num = 1;
+    int[] age = {3, 7, 0, 8, 4, 1, 9, 6, 5, 2};
+    int num2 = age[num];
+    System.out.println(age[num2]);
+  }
+}

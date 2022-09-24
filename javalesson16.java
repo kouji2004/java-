c82f04364f1eb766d1fce6f165432653a5c2e0e1
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-25 01:06:53 +0900

    java初級編_14

---
+++ javalesson16.java
@@ -0,0 +1,8 @@
+public class javalesson16 {
+  public static void main(String[]ages){
+    int num2 = 1;
+    int num1 = 3;
+    int[] array = {3, 7, 0, 8, 4, 1, 9, 6, 5, 2};
+    System.out.println(array[num1] * array[num2]);
+  }
+}

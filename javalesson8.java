b4b4d8a65993019a6c3a1f1b8c7da567b2c721ef
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-24 23:58:38 +0900

    java初級編_7

---
+++ javalesson8.java
@@ -0,0 +1,9 @@
+public class javalesson8 {
+  public static void main(String[]ages){
+    int num = 0;
+    for(int i=1; i<=10; i++){
+      num = num*i;
+    }
+    System.out.println(num);
+  }
+}
---
+++ javalesson9.java
@@ -0,0 +1,10 @@
+public class javalesson9 {
+  public static void main(String[] ages) {
+    int num1 = 2;
+    int num2 = 21;
+    int num3 = 223;
+    int num4 = 22;
+    int num5 = 278;
+    System.out.println(num1 + num2 + num3 + num4 + num5);
+  }
+}

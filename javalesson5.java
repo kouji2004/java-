2e39997c33afdbed9dd81f94a80c1b1090565b1a
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-24 23:16:05 +0900

    java初級編_4

---
+++ javalesson5.java
@@ -0,0 +1,12 @@
+public class javalesson5 {
+  public static void main(String[]ages){
+    int num = -10;
+    if(num < -10){
+      System.out.println("range");
+    }else if(num < 0 ){
+      System.out.println("rabge2");
+    }else{
+      System.out.println("range3");
+    }
+  }
+}

14754793e706622f02917f630132d1e1f938b0ab
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2022-09-24 21:28:15 +0900

    java初級編_2

---
+++ javalesson2.java
@@ -0,0 +1,11 @@
+public class javalesson2 {
+  public static void main(String[]ages){
+    System.out.println("練習問題");
+    int num = 6;
+       //　値が5よりも大きく、かつ、20よりも小さい場合
+    if(num>5 && num<20){
+      System.out.println("ok");
+    }
+    }
+  }
+
